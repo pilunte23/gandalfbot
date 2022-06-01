96b7c01d91aa72340268a3f12c4a8ca798d1c653
Pilunte <vad23@msn.com>
2022-06-01 16:16:42 +0200

    ajout mot cle

--- main.py
+++ main.py
@@ -17,11 +17,6 @@ load_dotenv(dotenv_path="config")
 bot = commands.Bot(command_prefix='!', case_insensitive=True)
 slash = SlashCommand(bot , sync_commands=True)
 
-def emoji(emoji_name):
-    emoji = discord.utils.get(bot.emojis, name=emoji_name)
-    str = emoji_name + f" {emoji}"
-    return str
-
 @bot.event
 async def on_ready():
 	""" check if bot is connected """
@@ -362,7 +357,7 @@ async def _selectingbox(ctx,resultat_carte):
         placeholder="choix de la carte",
         min_values=1,
         max_values=1
-    )   
+        )   
 
     fait_ctx = await ctx.send("Choisissez votre carte", components=[create_actionrow(select)])
 
@@ -440,5 +435,90 @@ async def sendcard(ctx,datacard):
     embed.set_image(url="attachment://image.jpg")
     embed.set_footer(text=f"{cycle}")
     await ctx.send(files=[file,pack_file], embed=embed)
+
+
+@slash.slash(
+    name="m",
+    description="Définition de mot clé",
+    guild_ids=list(map(int,str(os.getenv("GUILDID")).split(" "))),
+    options=[
+        create_option(
+            name="motcle",
+            description="mot-clé recherché",
+            required=True,
+            option_type=3
+        )
+    ]       
+)
+
+async def _mot(ctx:SlashContext, motcle:str):
+    "pas de multilingue pour l'instant"
+    langue="fr"
+    resultat_word = []
+    url_file =  "./data/keyword_"+langue+".json"
+    f = open(url_file)
+    data = json.load(f)
+    word_use = ".*"+ unidecode(str(motcle.lower()))+".*"
+    for i in data:
+        all_search = None
+        all_search = re.search(word_use,unidecode(str(i["word"].lower())))
+        if all_search:
+            resultat_word.append(i)   
+             
+    if len(resultat_word) > 0:
+        if len(resultat_word) == 1: 
+            datacard = resultat_word[0]
+            embed_word = discord.Embed(title=datacard['word'],color=discord.Color.green())
+            embed_word.add_field(name = "Définition", value = change(datacard['definition']))   
+            await ctx.send(embed = embed_word)
+        else:
+            if len(resultat_word) > 24:    
+                embed_too_word = discord.Embed(name = "too much result", color = discord.Color.red())
+                embed_too_word.add_field(name = "Trop de résultat", value = "Veuillez affiner votre recherche")   
+                too_card_msg = await ctx.send(embed = embed_too_word)
+                await asyncio.sleep(5)
+                await too_card_msg.delete()
+            else:
+                """menu for single word search"""
+                list_word = []
+                count = 0
+                for i in resultat_word:
+                    list_word.append(create_select_option(i['word'], value=str(count)))
+                    count += 1
+                    select = create_select(
+                    options=list_word,
+                    placeholder="choix du mot-clé",
+                    min_values=1,
+                    max_values=1
+                    )
+                fait_ctx = await ctx.send("Choisissez votre mot-clé", components=[create_actionrow(select)])
+                choice_ctx = await wait_for_component(bot,components=select)
+                datacard = resultat_word[int(choice_ctx.values[0])]
+                embed_word = discord.Embed(title=datacard['word'],color=discord.Color.green())
+                embed_word.add_field(name = "Définition", value = change(datacard['definition']))   
+                await ctx.send(embed = embed_word)
+                await fait_ctx.delete()
+    else:
+        file_url = "./assets/picture/no_card.png"
+        file = discord.File(file_url, filename="image.png")
+        embed_no_word = discord.Embed(name = "no result", color = discord.Color.red())
+        embed_no_word.set_image(url="attachment://image.png")
+        embed_no_word.add_field(name = "Ce mot-clé n'a été trouvé", value = "Vous ne passerez pas !!!")   
+        no_card_msg = await ctx.send(file=file,embed = embed_no_word)
+        await asyncio.sleep(5)
+        await no_card_msg.delete()
+
+def emoji(emoji_name):
+    emoji = discord.utils.get(bot.emojis, name=emoji_name)
+    return str(emoji)
+    
+
+def change(str):
+    str_with_emoji = str.replace("(defense)","("+emoji("defense") +")")
+    str_with_emoji = str_with_emoji.replace("(attaque)","("+ emoji("attack") +")")
+    str_with_emoji = str_with_emoji.replace("(volonte)","("+ emoji("willpower") +")")
+    str_with_emoji = str_with_emoji.replace("(menace)","("+ emoji("threat") +")")
+    str_with_emoji = str_with_emoji.replace("(pointdevie)","("+ emoji("hitpoints") +")")
+    return str_with_emoji
 
 bot.run(os.getenv("DISCORD_TOKEN"))
